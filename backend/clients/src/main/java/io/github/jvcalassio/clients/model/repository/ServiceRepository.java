3ce76074f08adedfae8af8236d7b3b03d395c7c1
João Victor <joao.victors1203@gmail.com>
2022-01-25 16:41:52 -0300

    backend folder removed

--- backend/clients/src/main/java/io/github/jvcalassio/clients/model/repository/ServiceRepository.java
+++
@@ -1,8 +0,0 @@
-package io.github.jvcalassio.clients.model.repository;
-
-import io.github.jvcalassio.clients.model.entity.Service;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServiceRepository extends JpaRepository<Service, Integer> {
-
-}
